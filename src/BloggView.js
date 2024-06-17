93013facafce1c3cc54667db17c9aec3e98106b4
Lukas Frösslund <lukas.frosslund@hotmail.com>
2024-06-17 13:13:48 +0200

    new blog

--- src/BloggView.js
+++ src/BloggView.js
@@ -4,6 +4,31 @@ const BloggView = () => {
     return (
         <div className="blogg-page">
             <div className="blogg-item">
+                <p id="timepublish">Publicerad: 17 juni 13:14 </p>
+                <hr />
+                <h2>En nation värdig ett mästerskap.</h2>
+
+                <p>
+                Tyskland. Det förlovade landet i syd. “Sverige för vuxna”. Här spelas EM-2024 och visst är det skönt att allt är samlat i ett och samma land.
+                <br /><br />
+                Förra Europamästerskapet spelades i jagvetintehurmånga-länder. Undertecknad, Wille Laahanen & C:o hade storartade planer på att åka husbil från land till land. Det var färja från Roscoff i Frankrike till Dublin. Biltur från Paris till Baku. Allt för att följa Sverige och bränna så mycket diesel som möjligt. Så blev det inte, en kinesisk snuva satte mästerskapet på paus. Slutspelet följdes på hemmaplan. Uppsalas 14e Nation abonnerades under Svea Rikes matcher. Det blev fest där också. I ett sista hoppfullt försök att få se stolta Gulo Gulo på plats så bokades en flygresa till Rom för att se Sverige - England. Kvartsfinal i EM på Stadio Olimpico. Det kunde inte bli bättre. Dessvärre bestämde sig Marcus Danielsson för att krossa även den drömmen och tomheten jag kände efter det svenska uttåget går inte ens att jämföra med dagen efter ett färgglatt Valborgsfirande i Uppsala. Serotonindepåer: tömda.
+                <br /><br />
+                Nåväl. Här står vi idag. Häggen har blommat ut, det lackar mot midsommar. Sverige är inte kvalificerat för EM. Och jag måste säga att det känns helt okej. I sommar kan vi lägga känslorna åt sidan och bara njuta av ett fullspäckat europeiskt mästerskap. Den sista riktiga generationens fotbollsspelare ska en sista gång dansa på den stora scenen. Toni Kroos, Thomas Müller, Luka Modrić, Pepe, Cristiano Ronaldo ska alla göra ett rejält försök att vinna en titel åt sitt land.
+                <br /><br />
+                EM-tipset då? Denna gigantiska koloss till tips som ingen förutom KTH-studenterna förstår hur det är uppbyggt. I skrivande stund, när första helgens matcher är spelade, leder Jonny på 34 poäng. Tvåa ligger Micke på 32 pinnar tätt skuggad av Alicia och Cajsa på en delad bronspeng.
+                <br /><br />
+                Det jag tar med mig från helgen är att hemmanationen, den värdiga, direkt satte ner foten och visade att tysk fotboll är att räkna med igen. Alla matcher är inte spelade i första omgången än men jag har svårt att se att något annat land ska kunna imponera lika stort som tyskarna gjorde. Kan det gå hela vägen? Det hoppas och tror undertecknad.
+                <br /><br />
+                Ungern-Schweiz spelades på lördagen och många som såg Magyarerna för tre år sedan trodde nog att de skulle kunna bygga vidare på sina senaste framgångar. Så blev det inte. Schweiz, med den självförtroendeuppumpade Leverkusen-generalen Granit Xhaka i spetsen vann utan snack.
+                <br /><br />
+                Italien vände och vann mot Albanien, en ny försvarsgeneral i Riccardo Calafiori visade upp sig för mainstreammedia. Spanien vann övertygande mot Kroatien, tänk vad bra det går när man spelar bollen i djupled.
+                <br /><br />
+                Helgens huvudnummer sparades till sist. England - Serbien. En match som många i det här landet var uppspelta inför. England levde upp till förväntningarna i en knapp halvlek. Högt bolltempo, snabb återerövring, fart och fläkt på kanterna. Alla tjejers favorit Jude Bellingham öppnade EM-kontot på ett mäktigt sätt. Det är kul att se att någon från den yngre generationen kan nicka. Efter paus sprakade det inte lika mycket om de tre lejonen. Serbien borde och skulle tagit en poäng i den här matchen och engelsmännen kan vara långt ifrån nöjda med spelet i den andra halvleken. Men vissa hävdar ju att man inte ska öppna för starkt i ett mästerskap för att kunna gå hela vägen. Italien vann dock sin premiär övertygande i förra europamästerskapet så det är väl vad det är med den “regeln”. Det återstår att se hur det går för England och det återstår att se hur det går i EM-tipset. Nu är det igång iallafall. Kulan är i luften. Undertecknad, som för närvarande parkerar på en tydlig sistaplats i tipset, vet att allt avgörs i finalen. Det är långt kvar. Men för guds skull se till att ta vara på den här tiden. Gruppspelstiderna är den bästa av tider, den värsta av tider, förnuftets tid, en tid av dumdristighet, en epok av tro, en epok av skepsis. Tre matcher om dagen i över en vecka. Se till att inte boka upp er på något under kommande veckorna. Sitt inne och kolla på fotboll.
+                <br /><br />
+                </p>
+                <p id="author"><i class="fas fa-pencil-alt"></i>: <strong>Per Kivijärvi</strong> <a href = "mailto: perolofkivijarvi@gmail.com"><i class="far fa-envelope"></i></a></p>
+            </div>
+            <div className="blogg-item">
                 <p id="timepublish">Publicerad: 14 juni 12:45 </p>
                 <hr />
                 <h2>Välkomna till EM-tippningens Värld – Låt Spelet Börja!</h2>
